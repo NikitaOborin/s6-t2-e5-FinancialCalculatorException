44a877df470e7fde97fe2a7d8c3adaa828593100
Oborin Nikita <oborinikita@yandex.ru>
2022-12-20 21:05:45 +0700

    задание из тренажера - Спринт 6, Тема 2 Исключения, Урок 5

---
+++ src/FinancialCalculatorException.java
@@ -0,0 +1,84 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class FinancialCalculatorException {
+    final static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // перехват исключения LimitException
+        try {
+            calculate();
+        } catch (LimitException exception) {
+            System.out.println("Превышен лимит ошибок ввода: " + exception.attempts);
+        }
+    }
+
+    public static double getInterest(final double rate, final int time, final double principal) {
+        final double multiplier = Math.pow(1.0 + rate / 100.0, time) - 1.0;
+        return multiplier * principal;
+    }
+
+    public static int getIntLimited(String greeting, int attempts) {
+        for (int counter = 0; counter < attempts; counter++) {
+            try {
+                System.out.println(greeting + " => ");
+                try {
+                    final int value = scanner.nextInt();
+                    // проверка на отрицательное значение
+                    // сгенерируйте исключение "Введено отрицательное значение"
+                    if (value < 0) {
+                        throw new InputException("Введено отрицательное значение");
+                    }
+                    return value;
+                } catch (InputMismatchException exception) {
+                    // сгенерируйте исключение "Введено не число"
+                    throw new InputException("Введено не число");
+                }
+            } catch (InputException exception) {
+                // сгенерируйте вывод формата "Ошибка ввода: " + информация об исключении
+                System.out.println("Ошибка ввода: ");
+                for (StackTraceElement stack : exception.getStackTrace()) {
+                    System.out.printf("%s.%s(%s:%d) %n", stack.getClassName(), stack.getMethodName(), stack.getFileName(), stack.getLineNumber());
+                }
+            }
+        }
+        // сгенерируйте исключение LimitException с сообщением "Превышен лимит ошибок ввода"
+        throw new LimitException("Превышен лимит ошибок ввода", attempts);
+    }
+
+    public static double getDoubleLimited(String greeting, int attempts){
+        for (int counter = 0; counter < attempts; counter++) {
+            try {
+                System.out.println(greeting + " => ");
+                // добавьте недостающий код
+                try {
+                    final double value = scanner.nextDouble();
+                    // проверка на отрицательное значение
+                    // сгенерируйте исключение "Введено отрицательное значение"
+                    if (value < 0) {
+                        throw new InputException("Введено отрицательное значение");
+                    }
+                    return value;
+                } catch (InputMismatchException exception) {
+                    // сгенерируйте исключение "Введено не число"
+                    throw new InputException("Введено не число");
+                }
+            } catch (InputException exception) {
+                // сгенерируйте вывод формата "Ошибка ввода: " + информация об исключении
+                System.out.println("Ошибка ввода: ");
+                for (StackTraceElement stack : exception.getStackTrace()) {
+                    System.out.printf("%s.%s(%s:%d) %n", stack.getClassName(), stack.getMethodName(), stack.getFileName(), stack.getLineNumber());
+                }
+            }
+        }
+        // сгенерируйте исключение LimitException с сообщением "Превышен лимит ошибок ввода"
+        throw new LimitException("Превышен лимит ошибок ввода", attempts);
+    }
+
+    public static void calculate () {
+        final double rate = getDoubleLimited("Введите ставку", 3);
+        final double principal = getDoubleLimited("Введите размер вклада", 3);
+        final int time = getIntLimited("Введите срок вклада в месяцах", 5);
+        System.out.println("Ваша выгода = " + getInterest(rate, time, principal));
+    }
+}
---
+++ src/LimitException.java
@@ -0,0 +1,8 @@
+public class LimitException extends RuntimeException {
+    final int attempts;
+
+    public LimitException(final String message, final int attempts) {
+        super(message);
+        this.attempts = attempts;
+    }
+}
